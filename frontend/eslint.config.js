1fd0e25cd5c05b741bc7bd169f79b14836ffc145
pythonPlant12 <pythonplant12@gmail.com>
2025-02-15 17:50:37 +0100

    - Added husky and new format for eslint and prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,51 +1,133 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 
 export default createConfigForNuxt({
-  files: ['**/*.{js,ts,vue}'],
+  files: ["**/*.{js,ts,vue}"],
   ignores: [
-    '**/*.config.js',
-    '.nuxt/*',
-    '.vscode/*',
-    'node_modules/*',
-    'public/*',
-    '.husky/*',
-    '**/*.d.ts',
-    '.nuxt/*',
-    '**/*.config.mjs',
-    '.eslintrc.js'
+    "**/*.config.js",
+    ".nuxt/*",
+    ".vscode/*",
+    "node_modules/*",
+    "public/*",
+    ".husky/*",
+    "**/*.d.ts",
+    ".nuxt/*",
+    "**/*.config.mjs",
+    ".eslintrc.js",
   ],
   rules: {
-    '@typescript-eslint/no-require-imports': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
+    "@typescript-eslint/no-require-imports": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports" },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
       {
-        endOfLine: 'auto',
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
         semi: false,
         singleQuote: true,
-        tabWidth: 2
-      }
+        tabWidth: 2,
+        bracketSpacing: true,
+        trailingComma: "es5",
+        printWidth: 100,
+        arrowParens: "always",
+        vueIndentScriptAndStyle: true,
+      },
     ],
-    semi: true
+    semi: true,
+    "template-curly-spacing": ["error", "always"],
+    "prefer-destructuring": [
+      "error",
+      {
+        array: true,
+        object: true,
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "object-curly-newline": [
+      "error",
+      {
+        ObjectExpression: { multiline: true, consistent: true },
+        ObjectPattern: { multiline: true, consistent: true },
+      },
+    ],
+    "callback-return": "error",
+    "require-await": "error",
+    "prefer-promise-reject-errors": "error",
+    "vue/multi-word-component-names": "off",
+    "vue/no-multiple-template-root": "off",
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "vue/html-closing-bracket-spacing": [
+      "error",
+      {
+        startTag: "never",
+        endTag: "never",
+        selfClosingTag: "always",
+      },
+    ],
+    "vue/html-indent": ["error", 2],
+    "vue/script-indent": [
+      "error",
+      2,
+      {
+        baseIndent: 0,
+        switchCase: 1,
+      },
+    ],
   },
   plugins: {
-    '@typescript-eslint': true,
-    'prettier': true
+    "@typescript-eslint": true,
+    prettier: true,
+    import: true,
+    vue: true,
   },
   languageOptions: {
     ecmaVersion: 2024,
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser',
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
     parserOptions: {
-      project: 'tsconfig.json',
-      tsconfigRootDir: '.',
-      sourceType: 'module',
+      project: "tsconfig.json",
+      tsconfigRootDir: ".",
+      sourceType: "module",
       ecmaFeatures: {
-        decorators: true
-      }
-    }
-  }
-})
+        decorators: true,
+        jsx: true,
+      },
+    },
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+});
